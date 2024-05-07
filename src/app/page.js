957b714131fdf6caae86bcb994cebb7c419ba359
Eray Chumak <eraychumak_@outlook.com>
2024-05-07 07:49:36 +0100

    Add profile, projects, and latest repos

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,120 @@
 import Image from "next/image";
+import Link from "next/link";
+import { Octokit } from "octokit";
 
-export default function Home() {
+import PFP from "./pfp.jpg";
+import MRC from "./mrc.png";
+import REDESIGN from "./redesign.png";
+import GH from "./gh.svg";
+
+export default async function Home() {
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_PERSONAL_TOKEN
+  });
+
+  const {
+    data: { login }
+  } = await octokit.rest.users.getAuthenticated();
+
+  const { data } = await octokit.rest.repos.listForUser({
+    username: login,
+    sort: "created"
+  });
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <>
+      <header className="container p-10">
+        <nav className="flex gap-5 flex-wrap">
+          <h1 className="font-bold">Eray Chumak</h1>
+          <Link className="hover:underline text-blue-600" href={"https://github.com/eraychumak/"}>Github</Link>
+          <Link className="hover:underline text-blue-600" href={"https://www.linkedin.com/in/eray-chumak/"}>LinkedIn</Link>
+          <Link className="hover:underline text-blue-600" href={"mailto:eraychumak_@outlook.com"}>eraychumak_@outlook.com</Link>
+        </nav>
+      </header>
+      <main className="lg:grid lg:grid-cols-2">
+        <section className="container flex flex-col p-10 gap-10">
+          <section className="container flex max-w-sm flex-col gap-5">
+            <Image src={PFP} className="max-w-48 rounded-lg w-auto" alt="Profile picture"/>
+            <h2 className="font-bold text-xl">Introduction</h2>
+            <p>
+              Hello, I&apos;m Eray. I live in North West England
+              and I&apos;m a frontend developer. I enjoy working
+              alone or with others to create useful web
+              apps. I&apos;m curious and pay close attention to
+              detail, thanks to having four eyes instead of two.
+            </p>
+          </section>
+          <section className="max-w-md">
+            <h2 className="font-bold mb-2 text-xl">Projects</h2>
+            <section className="container flex flex-wrap gap-10">
+              <article className="container flex max-w-m flex-col gap-5">
+                <h3 className="font-medium text-lg">Mortgage repayment calculator</h3>
+                <Image src={MRC} className="rounded-lg w-auto" alt="Profile picture"/>
+                <p>An open-source native web component that can be embeded on websites.</p>
+              </article>
+              <article className="container flex max-w-m flex-col gap-5">
+                <h3 className="font-medium text-lg">Local business redesign</h3>
+                <Image src={REDESIGN} className="rounded-lg w-auto border-2 rounded-xl" alt="Profile picture"/>
+                <p>
+                  A heuristic evaluation on a local business
+                  in Ormskirk, producing a new designs
+                  for the website.
+                </p>
+                <Link
+                  href={"https://www.figma.com/file/2Zkirrt2WdN1Ll6434QSvd/Large-Task%3A-Local-Business-Redesign?type=design&node-id=0%3A1&mode=design&t=cEaj9BxLdfFFuCft-1"}
+                  className="hover:underline text-blue-600"
+                >
+                  Preview Figma designs
+                </Link>
+                <Link
+                  href={"https://www.figma.com/file/UskxC56unTpOEQ79ofyJlm/Large-Task%3A-Local-Business-Redesign?type=whiteboard&node-id=0%3A1&t=XWWQsnT8bEnZSU93-1"}
+                  className="hover:underline text-blue-600"
+                >
+                  Preview FigJam (for heuristic evaluation)
+                </Link>
+              </article>
+            </section>
+          </section>
+        </section>
+        <section className="container flex flex-col p-10 gap-10">
+          <section>
+            <h2 className="font-bold text-xl mb-2">Skills</h2>
+            <ul>
+              <li>JavaScript</li>
+              <li>NodeJS</li>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>React & React Native</li>
+              <li>NextJS</li>
+              <li>Git</li>
+              <li>Firebase</li>
+              <li>AI</li>
+              <li>SEO</li>
+              <li>Figma</li>
+            </ul>
+          </section>
+          <section>
+            <h2 className="font-bold text-xl mb-2">See my latest repos</h2>
+            <section className="container flex flex-wrap gap-5">
+              {data.map((repo, i) => {
+                return (
+                  <article key={i} className="relative container justify-between flex flex-col gap-2 p-5 border rounded-lg max-w-sm">
+                    <Image src={GH} width={16} height={16} alt="Github logo" className="absolute top-4 right-4"/>
+                    <section>
+                      <p className="font-medium mb-2">{repo.name}</p>
+                      <p className="text-slate-700">{repo.description}</p>
+                    </section>
+                    <Link className="hover:underline text-blue-600 self-end" href={repo.html_url}>Visit repository</Link>
+                  </article>
+                )
+              })}
+            </section>
+          </section>
+        </section>
+      </main>
+      <footer className="p-5 border-t">
+        <p>Copyright &copy; 2024 Eray Chumak. All rights reserved.</p>
+      </footer>
+    </>
   );
 }
